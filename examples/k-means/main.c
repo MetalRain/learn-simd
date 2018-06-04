02ef525618db444885c69af8f3d398bb6ed3cdc8
Otto Martikainen <martikainenotto@gmail.com>
2018-06-04 07:53:22 +0100

    Show better statistics about results

--- examples/k-means/main.c
+++ examples/k-means/main.c
@@ -4,11 +4,38 @@
 #include <string.h>
 #include "dataset.h"
 
-int main() {
-  const int cluster_count = 4,
-    thread_count = 4;
+double min(int count, double* results){
+  double current = results[0];
+  for(int i=0; i < count; i++){
+    if (results[i] < current){
+      current = results[i];
+    }
+  }
+  return current;
+}
 
-  const int point_count = 32000 * 4,
+double max(int count, double* results){
+  double current = results[0];
+  for(int i=0; i < count; i++){
+    if (results[i] > current){
+      current = results[i];
+    }
+  }
+  return current;
+}
+
+double avg(int count, double* results){
+  double sum;
+  for(int i=0; i < count; i++){
+    sum += results[i];
+  }
+  return sum / count;
+}
+
+int main() {
+  const int cluster_count = 4, thread_count = 2;
+
+  const int point_count = 1000000,
     executions = 10;
 
   clock_t start, diff;
@@ -32,7 +59,7 @@ int main() {
   // k-means changes clusters, store initial values here
   memcpy(initial_values, clusters, cluster_count * sizeof(Cluster));
 
-  printf("Running all implementations %d times, please wait..\n", executions);
+  printf("Running all implementations with %d points and %d clusters..\n", point_count, cluster_count);
 
   for(int i=0; i < executions; i++) {
 
@@ -82,14 +109,10 @@ int main() {
     threaded_simd_results[i] = seconds;
   }
 
-
-  for(int i=0; i < executions; i++) {
-    printf(
-      "Run %d | LINEAR: %fs | THREADS: %fs | SIMD: %fs | SIMD + THREADS: %fs\n",
-      i, 
-      linear_results[i], threaded_results[i], simd_results[i], threaded_simd_results[i]
-    );
-  }
+  printf("LINEAR         - min: %gs, max: %gs, avg: %gs\n", min(executions, linear_results), max(executions, linear_results), avg(executions, linear_results));
+  printf("THREADS        - min: %gs, max: %gs, avg: %gs\n", min(executions, threaded_results), max(executions, threaded_results), avg(executions, threaded_results));
+  printf("SIMD           - min: %gs, max: %gs, avg: %gs\n", min(executions, simd_results), max(executions, simd_results), avg(executions, simd_results));
+  printf("SIMD + THREADS - min: %gs, max: %gs, avg: %gs\n", min(executions, threaded_simd_results), max(executions, threaded_simd_results), avg(executions, threaded_simd_results));
 
   free(xs);
   free(ys);
